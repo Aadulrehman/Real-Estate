bdbab7bc5523cdf2b4c7400e52ac7ae8f70e52f9
abdul_rehman <itzabdulrehman010@gmail.com>
2023-09-14 17:18:49 +0500

    Activity Life Cycle
    Observed the Life Cycle of Activity
    Implemented Log.d

--- app/src/main/java/com/hazel/internshipproject/HomePage.kt
+++ app/src/main/java/com/hazel/internshipproject/HomePage.kt
@@ -6,41 +6,40 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class HomePage : AppCompatActivity() {
     private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
 
-        Log.d("LifeCycle","OnCreate Act1")
+        Log.d("LifeCycle","OnCreate Act2")
         button=findViewById(R.id.btn)
         button.setOnClickListener{
-            startActivity(Intent(this,HomePage::class.java))
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
     override fun onStart() {
         super.onStart()
-        Log.d("LifeCycle","OnStart Act1")
+        Log.d("LifeCycle","OnStart Act2")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("LifeCycle","OnDestroy Act1")
+        Log.d("LifeCycle","OnDestroy Act2")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("LifeCycle","OnPause Act1")
+        Log.d("LifeCycle","OnPause Act2")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("LifeCycle","OnResume Act1")
+        Log.d("LifeCycle","OnResume Act2")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("LifeCycle","OnStop Act1")
+        Log.d("LifeCycle","OnStop Act2")
     }
-
 }
