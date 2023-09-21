fcf19d93fa1de1dd1b2f5b9823e17e2fe2e67bbd
abdul_rehman <itzabdulrehman010@gmail.com>
2023-09-21 17:53:04 +0500

    Task 5
    Added Radio Buttons and their kotlin implementations

--- app/src/main/java/com/hazel/internshipproject/AddPropertyActivity.kt
+++ app/src/main/java/com/hazel/internshipproject/AddPropertyActivity.kt
@@ -2,6 +2,7 @@ package com.hazel.internshipproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -13,29 +14,63 @@ import com.hazel.internshipproject.databinding.ActivityProfileBinding
 
 class AddPropertyActivity : AppCompatActivity() {
     private lateinit var viewBinder: ActivityAddPropertyBinding
-    lateinit var area:String
+    lateinit var area: String
+    lateinit var purpose:String
+    lateinit var interior:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewBinder= ActivityAddPropertyBinding.inflate(layoutInflater)
+        viewBinder = ActivityAddPropertyBinding.inflate(layoutInflater)
         setContentView(viewBinder.root)
 
         selectArea(viewBinder.spn)
+        viewBinder.rbNonFurnished.isChecked = true
+        viewBinder.rbForSale.isChecked=true
 
-
-
+        checkFurnished()
+        checkPurpose()
     }
-    fun selectArea(spn: Spinner) {
+
+    private fun selectArea(spn: Spinner) {
         val areaItems = resources.getStringArray(R.array.areaSpinner)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, areaItems)
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
 
         spn.adapter = adapter
         spn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ){
                 area = areaItems[position]
             }
             override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+    }
+    private fun checkFurnished() {
+        viewBinder.InteriorGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rbFurnished -> {
+                    interior=resources.getString(R.string.FurnishTag)
+                }
+                R.id.rbNonFurnished -> {
+                    interior=resources.getString(R.string.NonFurnishTag)
+                }
+            }
+        }
+    }
+    private fun checkPurpose() {
+        viewBinder.PurposeGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rbForSale -> {
+                    purpose=resources.getString(R.string.SaleTag)
+                }
+                R.id.rbForRent -> {
+                    purpose=resources.getString(R.string.RentTag)
+                }
             }
         }
     }
